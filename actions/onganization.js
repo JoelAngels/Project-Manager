cf7bb6c438e49427920844c79c77a22d87e4e69f
Joel Angel <joel.angel@gitfitcode.com>
2024-11-18 17:18:40 +0300

    feat: organization created

---
+++ actions/onganization.js
@@ -0,0 +1,48 @@
+"use server";
+
+import { db } from "@/lib/prisma";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+
+export async function getOrganization(slug) {
+  // get user from clerk auth
+
+  const { userId } = auth();
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  //Check if user exists in our database or not
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  //User does not exist
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  //Get organization details
+  const organization = await clerkClient().organizations.getOrganization({
+    slug,
+  });
+
+  if (!organization) {
+    return null;
+  }
+
+  //Check User if he or she is member of the organization
+  const { data: membership } =
+    await clerkClient().organizations.getOrganizationMembershipList({
+      organizationId: organization.id,
+    });
+
+  const userMembership = membership.find(
+    (member) => member.publicUserData.userId === userId
+  );
+
+  if (!userMembership) {
+    return null;
+  }
+
+  return organization;
+}
--- app/(main)/organization/[orgId]/page.tsx
+++ app/(main)/organization/[orgId]/page.tsx
@@ -1,10 +1,29 @@
+import { getOrganization } from "@/actions/onganization";
+import OrgSwitcher from "@/components/OrgSwitcher";
+
 interface Params {
   orgId: string;
 }
 
-const Organization = ({ params }: { params: Params }) => {
-  const { orgId } = params;
-  return <div>{orgId}</div>;
+const Organization = async ({ params }: { params: Params }) => {
+  const { orgId } = await params;
+  const organization = await getOrganization(orgId);
+  if (!organization) {
+    return <div>Organization not found</div>;
+  }
+  return (
+    <div className="container mx-auto">
+      <div className="mb-4 flex flex-col sm:flex-row justify-between items-start pt-10">
+        <h1 className="text-5xl font-bold gradient-title pb-2">
+          {organization.name}&rsquo;s Project
+        </h1>
+        {/* ORG Switcher */}
+        <OrgSwitcher />
+      </div>
+      <div className="mb-4">Show ORG Projects</div>
+      <div className="mt-8">Show user assigned and reported issues here</div>
+    </div>
+  );
 };
 
 export default Organization;
